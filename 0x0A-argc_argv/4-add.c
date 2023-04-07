356f638f6358f063abe004c92552abb89ec6bf6a
Jeffrey Baah <jbaah68@gmail.com>
2023-04-07 16:24:28 +0000

    add positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,7 +15,7 @@ int main (int argc, char *argv[])
 	{
 		for (y = 0; argv[x][y] != '\0'; y++)
 		{
-			if (!isdigit(arg[x][y]))
+			if (!isdigit(argv[x][y]))
 			{
 				printf("Error\n");
 				return (1);
