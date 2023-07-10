ba5308d1163c970d66361aa0b9cbb27a4cd8e5ed
Jeffrey Baah <jbaah69@gmail.com>
2023-07-10 16:26:12 +0000

    file i/o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,7 +9,7 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd; 
+	int fd;
 	int r;
 	char *buf = NULL;
 
@@ -18,8 +18,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (0);
-
+		return (0)
 	buf = malloc(letters);
 
 	if (buf == NULL)
