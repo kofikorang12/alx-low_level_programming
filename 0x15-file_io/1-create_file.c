2b00cc4beeb1866ca2c827e9c8eebe1e4dcd8c80
Kofi korang <jbaah69@gmail.com>
2024-03-04 23:20:58 +0000

    Updates of files

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,49 +1,34 @@
 #include "main.h"
 
 /**
- * _strlen - find length of string
- * @str: string to be evaluated
- *
- * Return: length of string
- */
-int _strlen(char *str)
-{
-	int i = 0;
-
-	while (*str != '\0')
-	{
-		i++;
-		str++;
-	}
-
-	return (i);
-}
-
-/**
  * create_file - creates a file
- * @filename: name of file to be created
- * @text_content: content to be written into file
+ * @filename: filename.
+ * @text_content: content writed in the file.
  *
- * Return: 1 if success, -1 if failure
+ * Return: 1 if it success. -1 if it fails.
  */
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int len;
+	int nletters;
+	int rwr;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-
-	if (fd < 0)
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
 		return (-1);
 
-	if (text_content == NULL)
-		return (1);
+	if (!text_content)
+		text_content = "";
 
-	len = _strlen(text_content);
-	if (write(fd, text_content, len) < len)
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
 		return (-1);
 
 	close(fd);
